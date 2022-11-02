4a3f429b0723c7982bd06614b9634facf1dfc9bc
İlayda Cundar <ilayda.cundar@akinon.com>
2022-11-02 15:02:49 +0300

    Created weather table

--- src/App.js
+++ src/App.js
@@ -3,29 +3,57 @@ import { useEffect, useState } from 'react';
 import { BASE_URL,CITY_1 } from './config/constants';
 
 function App() {
-  const [dayBaseData, setdayBaseData] = useState([]);
+  const [data, setdata] = useState(null);
+	const [filteredData, setfilteredData] = useState(null);
+	const [temp, setTemp] = useState('°C');
+
     useEffect(() => {
+
         let url = `${BASE_URL}/v1/forecast.json?key=698dc13c77094cf187695058201212&q=${CITY_1}&days=1`
-        fetch(url)
-        .then(response=>response.json())
-        .then(data => {
-            console.log(data,"data")
-            setdayBaseData(data)
-        });
+
+				setInterval(() => {
+					fetch(url)
+					.then(response=>response.json())
+					.then(responsedData => {
+						setdata(responsedData)
+						gethourly(responsedData)
+					});
+				}, 10000);
+			
     }, [])
+     
+		const gethourly = (data) => {
+			setfilteredData(data?.forecast.forecastday[0].hour.filter((item) => new Date(item.time) >= new Date()).slice(0,8))
+		}
+
+		const selectTemp = (selectedTemp) => {
+			if(selectedTemp !== temp ){
+				setTemp(selectedTemp);
+			}
+		}
 
   return (
-    <>
-    	<div>
-        {dayBaseData.forecast.forecastday.map = (item, index) => {
-          <div key={index}>
-              <p>12PM</p>
-              <img src={item.day.condition.icon} alt={item.day.condition.text}/>
-              <p>{item.day.avgtemp_c}</p>
-          </div>
-        }}
+    <div className='weather'>
+		  <div className='weather-header'>
+				<div>
+					<p className='weather-header__condition'>{data?.current?.condition.text}</p>
+					<p >{data?.location?.name}, {data?.location?.country}</p>
+				</div>
+				<div className='weather-header__temp'>
+					<span onClick={()=>selectTemp('°C')} className = {temp === '°C' ? 'activeTemp' : '' }>°C</span>
+					<span onClick={()=>selectTemp('°F')} className = {temp !== '°C' ? 'activeTemp' : '' }>°F</span>
+				</div>
+			</div>
+    	<div className='weather-content'>
+        {filteredData?.map((item, index) => 
+				  <div key={index} className="weather-content__icon-list">
+						<p>{new Date(item.time).getHours()>= 12 ? `${new Date(item.time).getHours()} PM` : `${new Date(item.time).getHours()} AM`}</p>
+						<img src={item.condition.icon} alt={item.condition.text}/>
+						<p>{temp === '°C' ? `°C ${item.temp_c}` : `°F ${item.temp_f}`}</p>
+					</div>
+				)}
     	</div>
-    </>
+    </div>
   );
 }
 export default App;
