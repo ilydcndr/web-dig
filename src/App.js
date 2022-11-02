8beda6471f7e9d6305c369b09e772e82e8378279
İlayda Cundar <ilayda.cundar@akinon.com>
2022-11-02 09:45:42 +0300

    Created componennt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { BASE_URL,CITY_1 } from './config/constants';
 
 function App() {
+  const [dayBaseData, setdayBaseData] = useState([]);
+    useEffect(() => {
+        let url = `${BASE_URL}/v1/forecast.json?key=698dc13c77094cf187695058201212&q=${CITY_1}&days=1`
+        fetch(url)
+        .then(response=>response.json())
+        .then(data => {
+            console.log(data,"data")
+            setdayBaseData(data)
+        });
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    	<div>
+        {dayBaseData.forecast.forecastday.map = (item, index) => {
+          <div key={index}>
+              <p>12PM</p>
+              <img src={item.day.condition.icon} alt={item.day.condition.text}/>
+              <p>{item.day.avgtemp_c}</p>
+          </div>
+        }}
+    	</div>
+    </>
   );
 }
-
 export default App;
---
+++ src/config/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.weatherapi.com'
+export const CITY_1 = 'London';
